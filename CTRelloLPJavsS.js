25a9cd07f153dae72294eddcca1e894610d3fa3b
leemo10 <leemo10@outlook.com>
2016-08-10 16:31:06 +0100

    Add files via upload

---
+++ CTRelloLPJavsS.js
@@ -0,0 +1,4 @@
+document.getElementById("newBoardBtn").onclick = function(){
+	window.location = "C:/Users/lem09/my_apps/L82m/CorePageData/CTrelloBoardPage.html";
+}
+
---
+++ CTRelloLP_JS.js
@@ -0,0 +1,4 @@
+function loadNewPage(){
+	console.log("clicked - navigating to input page");
+	window.location.href = 'CTrelloBoardPage.html';
+}
